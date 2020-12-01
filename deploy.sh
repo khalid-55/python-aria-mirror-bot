320d3357d82b256dd03103625e18ae180b49852e
khalid-55 <73981341+khalid-55@users.noreply.github.com>
2020-12-02 00:56:46 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ heroku create $1
 heroku git:remote -a $1
 heroku stack:set container
 heroku addons:create heroku-postgresql
-git add -f credentials.json token.pickle config.env heroku.yml
+git add -f credentials.json token.pickle config.env heroku.yml authorized_chats.txt
 git commit -m "Added Creds."
 git push heroku master --force
 heroku ps:scale worker=0
